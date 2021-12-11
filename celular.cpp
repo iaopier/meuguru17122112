2a07a78f18d5ab4a4927e2d3f5f2a00abf5c757f
Piercarlo Fermino Soares <pierhett@gmail.com>
2021-12-11 09:27:25 -0300

    Add files via upload

---
+++ celular.cpp
@@ -0,0 +1,30 @@
+#include "celular.hpp"
+
+// TODO: Inicialize aqui sua variavel estatica. Caso tenha optado por essa
+// solucao.
+
+Celular::Celular(std::string pmodelo,
+                 std::string pfabricante,
+                 int parmazenamento,
+                 int pmemoria,
+                 double ppeso,
+                 std::string pcor,
+                 int pqtd,
+                 float pvalor){
+                 this->modelo = pmodelo;
+                 fabricante = pfabricante;
+                 armazenamento = parmazenamento;
+                 memoria = pmemoria;
+                 peso = ppeso;
+                 cor = pcor;
+                 qtd = pqtd;
+                 valor = pvalor;
+}
+
+bool Celular::operator<(const Celular& other) {
+  // TODO: Implemente este metodo.
+  // Preste atencao no exemplo que a descricao utilizada na ordenacao considera
+  // a fabricante antes do modelo do celular
+
+  return false;
+}
---
+++ venda.cpp
@@ -0,0 +1,52 @@
+#include "venda.hpp"
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+void Venda::adicionaCelular(const Celular& celular) {
+    int list_size = m_celulares.size();
+    if(list_size == 0){
+        m_celulares.emplace_back(celular);
+    }
+}
+
+void Venda::ordena() {
+  // TODO: Implemente este metodo
+  // Preste atencao no exemplo que a descricao utilizada na ordenacao considera
+  // a fabricante antes do modelo do celular
+}
+
+void Venda::recarregaEstoque(int cod, int qtd) {
+  // TODO: Implemente este metodo
+}
+
+void Venda::efetuaVenda(int cod, int qtd) {
+  // TODO: Implemente este metodo
+  /**
+   * Observe na descricao do enunciado que nao e permita a venda de um modelo
+   * cuja quantidade seja maior que a quantidade atual em estoque.
+   * Ex: Se existirem apenas 5 celulares Motorola Moto G50 em estoque, vendas a
+   * partir de 6 unidades devem ser ignoradas.
+   */
+}
+
+void Venda::aplicaDesconto(const std::string& fabricante, float desconto) {
+  // TODO: Implemente este metodo
+}
+
+void Venda::removeModelo(int cod) {
+  // TODO: Implemente este metodo
+}
+
+void Venda::imprimeEstoque() const {
+  // TODO: Implemente este metodo.
+  /**
+   * Aqui voce tera que imprimir a lista de celulares em estoque com as
+   * restricoes descritas no enunciado do problema. Notem no exemplo que a
+   * primeira letra da cor do celular fica maiuscula. Outra coisa, se houver
+   * apenas um item em estoque de determinado modelo de celular, voce tera que
+   * imprimir '1 restante' e nao '1 restantes'. Por ultimo, apenas modelos de
+   * celulares com pelo menos um item em estoque deverao ser exibidos.
+   */
+}
