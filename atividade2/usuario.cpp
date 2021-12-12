4843feb1d3a7d0ee4bd890dc930120168545a03b
Piercarlo <pierhett@gmail.com>
2021-12-12 08:55:18 -0300

    first commit

---
+++ atividade2/usuario.cpp
@@ -0,0 +1,23 @@
+#include "usuario.hpp"
+
+Usuario::Usuario(const std::string& login, const std::string& password) {
+  // TODO: Implemente este metodo
+}
+
+std::string Usuario::getLogin() const {
+  // TODO: Implemente este metodo
+  return "";
+}
+
+std::string Usuario::getPassword() const {
+  // TODO: Implemente este metodo
+  return "";
+}
+
+void Usuario::setPassword(const std::string& password) {
+  // TODO: Implemente este metodo
+}
+
+void Usuario::setLogin(const std::string& login) {
+  // TODO: Implemente este metodo
+}
