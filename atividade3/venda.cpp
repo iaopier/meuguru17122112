b0bd266978aec28027fd737e41da958a79a27146
Piercarlo <pierhett@gmail.com>
2021-12-13 16:18:12 -0300

    change number decimal

--- atividade3/venda.cpp
+++ atividade3/venda.cpp
@@ -1,5 +1,6 @@
 #include "venda.hpp"
-
+#include <iomanip>
+#include <iostream>
 Venda::~Venda() {
     for(auto pedido: m_pedidos){
         delete(pedido);
@@ -14,7 +15,7 @@ void Venda::adicionaPedido(Pedido* p) {
 
 void Venda::imprimeRelatorio() const {
     int contador = 0;
-    float total = 0.0;
+    double total = 0.0;
     for(auto pedido : this->m_pedidos){
         std::cout << "Pedido " << contador + 1 << std::endl;
         std::cout << pedido->resumo();
@@ -22,7 +23,7 @@ void Venda::imprimeRelatorio() const {
         total = total + pedido->calculaTotal();
     }
     std::cout << "Relatorio de vendas" << std::endl;
-    std::cout << "Total de Vendas: R$ " << total << std::endl;
+    std::cout << "Total de Vendas: R$ " << std::fixed << std::setprecision(2) << total << std::endl;
     std::cout << "Total de Pedidos: " << contador;
 
 }
